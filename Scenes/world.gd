54ddc48c4c9b30e95058791370767cdbb72eda8a
Kevin Bertram <kbrtrm@gmail.com>
2024-04-15 13:00:49 -0700

    Changed dirt color, removed old relfection logic

--- Scenes/world.gd
+++ Scenes/world.gd
@@ -5,28 +5,7 @@ extends Node2D
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$YSort/Player/Reflection2.connect("texture_loaded", , "_on_texture_loaded")
-	
-func _on_texture_loaded():
-	# Get the player sprite texture (assuming it's set somewhere)
-	var player_sprite_texture = reflection_texture.texture.get_data()
-
-	# Create a viewport for rendering the reflection
-	var viewport = SubViewport.new()
-	viewport.render_target_v_flip = true # Flip vertically
-	add_child(viewport)
-
-	# Create a texture rect inside the viewport to display the reflection
-	var reflection_texture = TextureRect.new()
-	viewport.add_child(reflection_texture)
-
-	# Set the shader material for the reflection texture
-	var material = ShaderMaterial.new()
-	material.shader = load("res://Scenes/PuddleTest3.gdshader")
-	reflection_texture.material = material
-
-	# Pass the player sprite texture to the shader
-	material.set_shader_param("player_texture", player_sprite_texture)
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
